adda88d880e2568ae4c4fe45d8bc7dac4c9c53de
Kevin Anderson <kmichael419@hotmail.com>
2020-10-12 12:02:22 -0500

    adding conditional to make sure a parameter is passed as an argument on run

--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -6,7 +6,14 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Hello, {args[0]}!");
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
         }
     }
 }
