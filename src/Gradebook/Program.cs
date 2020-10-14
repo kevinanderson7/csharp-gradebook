58f8f624e95dda4f2f3dececbc5ba54b82a8068e
Kevin Anderson <kmichael419@hotmail.com>
2020-10-14 16:22:22 -0500

    creating List and initializing with floating numbers and then looping through numbers and returning the average grade

--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Gradebook
 {
@@ -6,12 +7,21 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
-            var x = 34.1;
-            var y = 10.3;
 
-            var result = x + y;
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
 
-            Console.WriteLine(result);
+
+
+            var result = 0.0;
+            foreach (var number in grades)
+            {
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
+
+
 
             if (args.Length > 0)
             {
