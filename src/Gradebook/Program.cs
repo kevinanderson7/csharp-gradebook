846841fbe01f6b65dc265a9e4497a1082ab74870
Kevin Anderson <kmichael419@hotmail.com>
2020-10-14 15:39:56 -0500

    adding in variables with type double and displaying result of addition

--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -6,6 +6,13 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
+            var x = 34.1;
+            var y = 10.3;
+
+            var result = x + y;
+
+            Console.WriteLine(result);
+
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
